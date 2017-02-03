8882ba47ce4f2181410963f70055b2de20a00548
bj291264 <bj291264@C32649.ens.uqam.ca>
2017-02-02 22:10:44 -0500

    Ajout de 'modifiee'

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ajout dans Git");
+        System.out.println("ajout dans Git modifiee");
         System.out.println("Allo");
         System.out.println("Allo2");
     }
